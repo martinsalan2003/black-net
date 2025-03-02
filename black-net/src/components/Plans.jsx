218b7a62ddeca95dc3348a7710c8be8c56535d98
AlanMartins00 <alanmartinsferreira59@gmail.com>
2025-03-02 19:27:13 -0300

    troca componente card

--- black-net/src/components/Plans.jsx
+++ black-net/src/components/Plans.jsx
@@ -1,63 +1,101 @@
-import { useState } from "react";
 import '../styles/styles-components/Plans.sass';
 
 const plans = [
-    { title: "Essencial", price: "79,90", strinngLeftValue:'R$', stringRithValue:'/mês', speed: "300", stringRithMb:'Mbps', description: "Ideal para quem está começando." },
-    { title: "Ultra", price: "139,90", strinngLeftValue: 'R$', stringRithValue:'/mês', speed: "1000", stringRithMb:'Mbps', description: "Para quem deseja recursos avançados." },
-    { title: "Avançado", price: "99,90", strinngLeftValue:'R$', stringRithValue:'/mês',  speed: "700", stringRithMb:'Mbps', description: "Melhor custo-benefício para usuários regulares." }
-];
+    { 
+        title: "Essencial", 
+        price: "79,90", 
+        stringLeftValue: 'R$', 
+        stringRightValue: '/mês', 
+        speed: "300", 
+        stringRightMb: 'Mbps', 
+        features: [
+            "Acesso ilimitado",
+            "Suporte Tecnico",
+            "Aparelho 5G",
+            "Zenix TV",
+            "Wifi - 6",
+            "Telefonia - Gratis"
+        ],
+        availableFeatures: 4
+    },
 
-const features = [
-    { name: "Acesso Público", essential: true, advanced: true, ultra: true },
-    { name: "Acesso Privado", essential: false, advanced: true, ultra: true },
-    { name: "Permissões Avançadas", essential: true, advanced: true, ultra: true },
-    { name: "Compartilhamento", essential: false, advanced: true, ultra: true },
-    { name: "Membros Ilimitados", essential: false, advanced: true, ultra: true },
-    { name: "Segurança Extra", essential: false, advanced: false, ultra: true }
+    { 
+        title: "Ultra",
+        price: "139,90", 
+        stringLeftValue: 'R$', 
+        stringRightValue: '/mês', 
+        speed: "1000", 
+        stringRightMb: 'Mbps', 
+        features: [
+            "Acesso ilimitado",
+            "Suporte Tecnico",
+            "Aparelho 5G",
+            "Zenix TV",
+            "Wifi - 6",
+            "Telefonia - Gratis"
+        ],
+        availableFeatures: 6
+    },
+
+    { 
+        title: "Avançado", 
+        price: "99,90", 
+        stringLeftValue: 'R$', 
+        stringRightValue: '/mês',  
+        speed: "700", 
+        stringRightMb: 'Mbps', 
+        features: [
+            "Acesso ilimitado",
+            "Suporte Tecnico",
+            "Aparelho 5G",
+            "Zenix TV",
+            "Wifi - 6",
+            "Telefonia - Gratis"
+        ],
+        availableFeatures: 3
+    }
 ];
 
 export default function Plans() {
-    const [activeCard, setActiveCard] = useState(null);
-
     return (
         <section className="pricing-section">
             <h2>Escolha seu Plano</h2>
-            <p>Aqui estão alguns dos diversos planos que oferecemos, Escolha o plano de internet ideal para você e aproveite ao máximo!</p>
+            <p>Aqui estão alguns dos diversos planos que oferecemos, escolha o plano de internet ideal para você e aproveite ao máximo!</p>
             
             <div className="card-container">
                 {plans.map((plan, index) => (
-                    <div
-                        key={index}
-                        className={`card ${activeCard === index ? "hovered" : ""}`} 
-                        onMouseEnter={() => setActiveCard(index)}
-                        onMouseLeave={() => setActiveCard(null)}
-                        style={{ transition: "all 0.3s ease-in-out", transform: activeCard === index ? "scale(1.05)" : "scale(1)", boxShadow: activeCard === index ? "0px 4px 20px rgba(0, 0, 0, 0.2)" : "none" }}
-                    >
+                    <div key={index} className="card">
                         <div className="card-body">
-                            {activeCard === index ? (
-                                <>
-                                    <h3 className="card-title2">Bela escolha!</h3>
-                                    <p className="card-text-add">
-                                        <a href="/agendar" className="card-link">Clique aqui e agende sua instalação!</a>
-                                    </p>
-                                </>
-                            ) : (
-                                <>
-                                    <div className="cardheader">
-                                        <h3 className="card-title">{plan.title}</h3>
-                                        <p className="card-speed"><span>{plan.speed}</span>{plan.stringRithMb}</p>
-                                    </div>
-                                    <p className="card-subtitle">
-                                        {plan.strinngLeftValue} <span>{plan.price}</span> {plan.stringRithValue}
-                                    </p>
-                                    <p className="card-text">{plan.description}</p>
-                                </>
-                            )}
-                        </div>
+                            <div className="cardheader">
+                                <h3 className="card-title">{plan.title}</h3>
+                                <hr className="featurette-divider divisor" />
+                                <p className="card-speed">
+                                    <span>{plan.speed}</span> {plan.stringRightMb}
+                                </p>
+
+                                <p className="card-subtitle">
+                                    {plan.stringLeftValue} <span>{plan.price}</span> {plan.stringRightValue}
+                                    <hr className="featurette-divider divisor" />
+                                </p>
+                            </div>
+                            
+                            <div className="container-features">
+                                <ul className="card-features">
+                                    {plan.features.map((feature, idx) => (
+                                        <li key={idx} className={idx >= plan.availableFeatures ? "disabled" : ""}>
+                                            {idx >= plan.availableFeatures ? <s>{feature}</s> : feature}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+
+                            <div className='container-btn'>
+                                <a href="#"><button type='button'>Contratar!</button></a>
+                            </div>
+                        </div> 
                     </div>
                 ))}
             </div>
-
         </section>
     );
 }
--- black-net/src/components/Slogan.jsx
+++ black-net/src/components/Slogan.jsx
@@ -3,7 +3,7 @@ import '../styles/styles-components/Slogan.sass'
 export default function Slogan(){
     return(
 
-        <section className="container slogan">
+        <section className=" slogan">
             <h1>Black<span>Net.</span></h1>
             <p>Exclusividade e potência na sua conexão, navegue sem limites!</p>
 
